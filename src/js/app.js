24cce2ddba8051dba5e768299a40bc313410c508
Anastasia Shmeleva <shmeleva-nastya@mail.ru>
2021-08-23 18:00:23 +0300

    init

---
+++ src/js/app.js
@@ -0,0 +1,29 @@
+// TODO: write code here
+export default function game() {
+  const boardSize = 16;
+
+  const randomizer = () => Math.floor(1 + Math.random() * (boardSize - 1));
+  const getHole = (index) => document.getElementById(index);
+
+  document.addEventListener('DOMContentLoaded', () => {
+    const random = randomizer();
+    getHole(random).classList.add('hole__has-mole');
+  });
+
+  const moleMove = () => {
+    let random = randomizer();
+    const current = document.querySelector('.hole__has-mole');
+    const currentIndex = current.id;
+    if (random === Number(currentIndex)) {
+      random = randomizer();
+    }
+    current.classList.remove('hole__has-mole');
+    getHole(random).classList.add('hole__has-mole');
+  };
+
+  setInterval(() => {
+    moleMove();
+  }, 500);
+}
+
+game();
